6752089308e06448e334bea3bb639b1cb1719a90
murderteeth <murdertxxth@gmail.com>
2026-01-18 04:57:25 +0000

    Use user-provided cat meme image

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main
       className="flex min-h-screen flex-col items-center justify-center p-24 bg-cover bg-center bg-no-repeat"
       style={{
-        backgroundImage: "url('https://i.imgflip.com/ahayfe.gif')",
+        backgroundImage: "url('https://i.imgflip.com/ahb5hm.jpg')",
       }}
     >
       <div className="text-6xl font-bold text-white drop-shadow-lg">Dummy</div>
