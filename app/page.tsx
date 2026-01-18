c91ebc5245283123006aba3d9629c5a559bbb755
murderteeth <murdertxxth@gmail.com>
2026-01-18 04:53:16 +0000

    Use imgur hosted cat meme

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main
       className="flex min-h-screen flex-col items-center justify-center p-24 bg-cover bg-center bg-no-repeat"
       style={{
-        backgroundImage: "url('https://www.grumpycats.com/images/grumpycat.png')",
+        backgroundImage: "url('https://i.imgur.com/VtNO7n6.jpeg')",
       }}
     >
       <div className="text-6xl font-bold text-white drop-shadow-lg">Dummy</div>
